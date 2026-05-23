f40ab65700c9c5b5e358cad002b496cd154fc12e
dirkwa <dirkwa@users.noreply.github.com>
2026-05-23 20:07:49 +0000

    deploy: v0.1.0-8-ge8d5181 from e8d51811aa226b3c4d7a246c8a22052d46a87a8b e8d51811aa226b3c4d7a246c8a22052d46a87a8b

--- installer/windows/install.ps1
+++ installer/windows/install.ps1
@@ -14,7 +14,7 @@ param(
     [string]$InstallerBaseUrl = 'https://dirkwa.github.io/signalk-universal-installer'
 )
 
-if (-not $InstallerVersion) { $InstallerVersion = if ($env:INSTALLER_VERSION) { $env:INSTALLER_VERSION } else { 'v0.1.0-7-g4ffe24e' } }
+if (-not $InstallerVersion) { $InstallerVersion = if ($env:INSTALLER_VERSION) { $env:INSTALLER_VERSION } else { 'v0.1.0-8-ge8d5181' } }
 
 $ErrorActionPreference = 'Stop'
 
